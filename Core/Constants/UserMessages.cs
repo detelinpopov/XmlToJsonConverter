214f6acd60a8819f5f73527c41ed9bcc01bacd7e
detelinpopov <Popov.D@eppendorf.de>
2023-12-11 16:07:27 +0200

    Added user message.

--- Core/Constants/UserMessages.cs
+++ Core/Constants/UserMessages.cs
@@ -5,4 +5,6 @@ public static class UserMessages
     public static string InvalidXmlFileMessage = "The provided file is not a valid XML document.";
 
     public static string FileConvertedAndUploaded = "File Converted and Uploaded Successfully.";
+
+    public static string SaveJsonFileError = "An error occurred while saving the JSON file.";
 }
--- Core/Services/FileUploadService.cs
+++ Core/Services/FileUploadService.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Enums;
+﻿using Core.Constants;
+using Domain.Enums;
 using Domain.Models;
 using Interfaces.Services;
 
@@ -30,7 +31,7 @@ public class FileUploadService : IFileUploadService
                 Success = false,
                 Errors = new List<ErrorModel>
                 {
-                    new(){ ErrorMessage = "An error occurred while saving the JSON file.", ErrorType = ErrorType.UploadFile}
+                    new(){ ErrorMessage = UserMessages.SaveJsonFileError, ErrorType = ErrorType.UploadFile}
                 }
             };
 
